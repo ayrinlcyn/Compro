e3e51c7962ccbf2e22c65413af7710528d19f3b9
NaufalDanadyaksa <naufal060906@gmail.com>
2024-11-14 11:31:21 +0700

    update : connect to api

--- src/Work/ImageCard.js
+++ src/Work/ImageCard.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const ImageCard = ({ image }) => {
   return (
-    <Link to={`/image/${image.id}`} className="overflow-hidden m-0 w-full h-full">
-      <img src={image.src} alt={image.title} className="w-full h-[450px] md:h-[750px] object-cover cursor-pointer hover:opacity-80" />
+    <Link to={`/image/${image.contentId}`} className="overflow-hidden m-0 w-full h-full">
+      <img src={'http://localhost:8000'+image.src} alt={image.title} className="w-full h-[450px] md:h-[750px] object-cover cursor-pointer hover:opacity-80" />
     </Link>
   );
 };
